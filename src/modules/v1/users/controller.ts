ec55183cd4c572644fc6af6d26204da62d7001ce
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-23 19:07:27 +0530

    refactor: simplify user bookings retrieval by removing unnecessary user lookup

--- src/modules/v1/users/controller.ts
+++ src/modules/v1/users/controller.ts
@@ -1,8 +1,8 @@
 // internal-imports
-import { ErrorResponse, prisma, SuccessResponse } from '../../../core/index.js';
+import { prisma, SuccessResponse } from '../../../core/index.js';
 
 // type-imports
-import type { ISuccessResponse, IErrorResponse } from '../../../core/index.js';
+import type { ISuccessResponse } from '../../../core/index.js';
 import type { Request, Response } from 'express';
 
 // controller for module
@@ -10,29 +10,19 @@ export const controller = {
   // @controller GET /:id/bookings
   getUserBookings: async (
     request: Request,
-    response: Response<ISuccessResponse<Array<object>> | IErrorResponse<null>>
+    response: Response<ISuccessResponse<Array<object>>>
   ) => {
-    // find user with given id and select only bookings
-    const existingUser = await prisma.user.findUnique({
-      where: { id: Number(request.params.id) },
-      select: { bookings: true },
+    // find bookings of user with given id
+    const existingUserBookings = await prisma.booking.findMany({
+      where: { userId: Number(request.params.id) },
+      include: { event: true },
     });
-
-    // if user not found, send ErrorResponse
-    if (!existingUser)
-      return response.status(404).json(
-        new ErrorResponse<null>({
-          message: 'User not found',
-          code: 'USER_NOT_FOUND',
-          issues: null,
-        })
-      );
 
     // send SuccessResponse with user bookings
     return response.status(200).json(
       new SuccessResponse<Array<object>>({
         message: 'User bookings retrieved successfully',
-        data: existingUser.bookings,
+        data: existingUserBookings,
       })
     );
   },
